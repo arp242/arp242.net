078f5583346c20a3c1fed47e728a156a3c22badc
Martin Tournoij <martin@arp242.net>
2019-04-23 23:00:00 +1200

    Better table styles

--- _plugins/header.rb
+++ _plugins/header.rb
@@ -4,5 +4,7 @@ class Jekyll::MarkdownHeader < Jekyll::Converters::Markdown
 			.gsub(/<h(\d) id="(.*?)">(.*?)<\/h\d>/, '<h\1 id="\2">\3 <a href="#\2"></a></h\1>')
 			.gsub(/<div class="footnotes">/, '<div class="postscript"><strong>Footnotes</strong>')
 			.gsub(/<a href="#fn:(\d+)" class="footnote">\d+<\/a>/, '<a href="#fn:\1" class="footnote">[\1]</a>')
+			.gsub(/<t([dh]) style="text-align: (left|right|center)">/, '<t\1 class="\2">')
+			.gsub(/<\/table>\n+<p>Table: (.*?)<\/p>/, '<caption>\1</caption></table>')
 	end
 end
