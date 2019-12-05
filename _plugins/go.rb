4ffcf00237853ec15a4854d39910520e19e94f2c
Martin Tournoij <martin@arp242.net>
2019-12-05 18:44:49 +0800

    Add colorcount

--- _plugins/go.rb
+++ _plugins/go.rb
@@ -4,7 +4,7 @@ module Jekyll
 
     def generate(site)
       pkgs = %w{sconfig autofox goimport goimport/goimport hubhub info
-        orgstat singlepage trackwall transip-dynamic uni gosodoff}
+        orgstat singlepage trackwall transip-dynamic uni gosodoff, colorcount}
       pkgs.each do |p|
         site.pages << GoPage.new(site, site.source, p)
       end
