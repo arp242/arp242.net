0b354bb0eb09008a065c02ca3ee38e9f8c542402
Martin Tournoij <martin@arp242.net>
2020-02-11 15:47:38 +0800

    Add har

--- _plugins/go.rb
+++ _plugins/go.rb
@@ -5,7 +5,7 @@ module Jekyll
     def generate(site)
       pkgs = %w{sconfig autofox goimport goimport/goimport hubhub info orgstat
         singlepage trackwall transip-dynamic uni gosodoff colorcount alwayscache
-        cantuse border}
+        cantuse border har har/cmd/unhar}
       pkgs.each do |p|
         site.pages << GoPage.new(site, site.source, p)
       end
@@ -21,6 +21,8 @@ module Jekyll
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'go.html')
+      slash = p.index '/'
+      p = p[0..slash-1] unless slash.nil?
       self.data['pkg'] = p
     end
   end
