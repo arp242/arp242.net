3982ebb134e4f69a1a3d3dd9200e4e6e5fe5674f
Martin Tournoij <martin@arp242.net>
2020-06-04 03:20:58 +0800

    Add "Copy" button for <pre> tags with a scrollbar

--- _plugins/markdown.rb
+++ _plugins/markdown.rb
@@ -6,5 +6,7 @@ class Jekyll::MarkdownHeader < Jekyll::Converters::Markdown
       .gsub(/<a href="#fn:(.+?)" class="footnote">(\d+)<\/a>/, '<a href="#fn:\1" class="footnote">[\2]</a>')
       .gsub(/<t([dh]) style="text-align: (left|right|center)">/, '<t\1 class="\2">')
       .gsub(/<\/table>\n+<p>Table: (.*?)<\/p>/, '<caption>\1</caption></table>')
+      .gsub(/<pre(.*?)>/, "<div class='pre-wrap'>\n<pre\\1>")
+      .gsub(/<\/pre>/, "</pre>\n</div>")
   end
 end
